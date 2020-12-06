d8afc609cf233df2e8c4c2ead1c9a16c81db4a93
KevinLADLee <kevinladlee@gmail.com>
2020-12-06 17:00:14 +0800

    update scripts

--- docker/scripts/carla_ego.sh
+++ docker/scripts/carla_ego.sh
@@ -4,8 +4,12 @@ source /opt/ros/noetic/setup.bash
 source /opt/carla-ros-bridge/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
 
-roslaunch carla_2d_nav carla_move_base.launch & 
+roslaunch carla_2d_nav carla_ego.launch & 
 
 sleep 3
 
-python3 /opt/carla/PythonAPI/examples/spawn_npc.py -n 30 
+python3 /opt/carla/PythonAPI/examples/spawn_npc.py -n 30  &
+
+sleep 3 
+
+rostopic pub /carla/ego_vehicle/enable_autopilot std_msgs/Bool "data: true"
