107f2a451bdea171967673cb7438d2281f756644
KevinLADLee <kevinladlee@gmail.com>
2021-04-12 15:28:56 +0800

    update docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t harbor.isus.tech/carla-ros/carla-ros:0.9.10.1-noetic -f Dockerfile ./..
+docker build --network=host -t harbor.isus.tech/carla-ros/carla-ros:0.9.11 -f Dockerfile ./..
--- docker/scripts/carla_ego.sh
+++ docker/scripts/carla_ego.sh
@@ -4,12 +4,12 @@ source /opt/ros/noetic/setup.bash
 source /opt/carla-ros-bridge/devel/setup.bash
 export PYTHONPATH=$PYTHONPATH:/opt/carla/PythonAPI/carla/dist/carla-0.9.10-py3.7-linux-x86_64.egg
 
-roslaunch carla_2d_nav carla_ego.launch & 
+roslaunch carla_2d_nav carla_example_ego_vehicle.launch & 
 
 sleep 3
 
 python3 /opt/carla/PythonAPI/examples/spawn_npc.py -n 30  &
 
-sleep 3 
+# sleep 3 
 
-rostopic pub /carla/ego_vehicle/enable_autopilot std_msgs/Bool "data: true"
+# rostopic pub /carla/ego_vehicle/enable_autopilot std_msgs/Bool "data: true"
