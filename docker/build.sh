ad8e77227fcd23ad316ccc6bd16414ac3c372e2c
KevinLADLee <kevinladlee@gmail.com>
2021-09-14 14:01:53 +0800

    fixed docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker pull harbor.isus.tech/osrf/ros:noetic-desktop
-docker pull harbor.isus.tech/carlasim/carla:0.9.11
-docker build --network=host -t harbor.isus.tech/carla-ros/carla-ros:0.9.11 -f Dockerfile ./..
+docker pull harbor.isus.tech/carlasim/carla:0.9.12
+docker build --network=host -t harbor.isus.tech/carla-ros/carla-ros:0.9.12 -f Dockerfile ./..
